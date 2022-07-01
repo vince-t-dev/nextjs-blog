0ad6284bb3935129f815d6648a449b1e59e4e6b0
Vince T <vince@Blackie.local>
2022-07-01 15:51:24 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig = {
     // use expresia path for production and localhost for development.
-    assetPrefix: isProd ? "/__xpr__/pub_engine/next/web" : ""
+    assetPrefix: isProd ? "/__xpr__/pub_engine/nextjs-blog/web" : ""
 }
   
 module.exports = nextConfig;
